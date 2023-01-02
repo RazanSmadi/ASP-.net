91c0a001e201c6c6b00261d5cc367b9b70338248
razansmadi99@gmail.com <razansmadi99@gmail.com>
2023-01-02 21:04:33 +0300

    t

---
+++ 2.1.2023/WebUserControl1.ascx
@@ -0,0 +1,22 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="WebUserControl1.ascx.cs" Inherits="_2._1._2023.WebUserControl1" %>
+
+
+<div  id="divv" runat="server"  Visible="true"> 
+    
+<asp:Label ID="Label1" runat="server" Text="">TTTTTTTTTTTTTTTTT?</asp:Label>
+       <asp:RadioButtonList ID="RadioButtonList1" runat="server" CellSpacing="70"  RepeatDirection="Vertical">
+                    <asp:ListItem >Exellent</asp:ListItem>
+                    <asp:ListItem >V.good</asp:ListItem>
+                    <asp:ListItem >Good</asp:ListItem>
+                    <asp:ListItem >Poor</asp:ListItem>
+                </asp:RadioButtonList>
+<asp:Button ID="Button1" runat="server" Text="vote" OnClick="Button1_Click" />
+    </div>
+<div  id="div" runat="server"  Visible="false"> 
+<asp:Label ID="Label2" runat="server" Text="">TTTTTTTTTTTTTTTTT?</asp:Label><br />
+<asp:Label ID="exx" runat="server" Text="Exellent"></asp:Label><br />
+<asp:Label ID="vgx" runat="server" Text="V.good"></asp:Label><br />
+<asp:Label ID="gx" runat="server" Text="Good"></asp:Label><br />
+<asp:Label ID="px" runat="server" Text="poor"></asp:Label>
+
+</div>
---
+++ 2.1.2023/WebUserControl1.ascx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _2._1._2023
+{
+    public partial class WebUserControl1 : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            HttpCookie cookie = new HttpCookie("vote");
+            cookie.Values.Add("uservote", RadioButtonList1.SelectedValue);
+
+
+                Response.Cookies.Add(cookie);
+                cookie.Expires = DateTime.Now.AddMinutes(60);
+
+            int ex=0, g=0, vg=0, p=0;
+            if (RadioButtonList1.SelectedValue == "Exellent") 
+            {
+                ex++;
+            }
+            if (RadioButtonList1.SelectedValue == "V.good")
+            {
+                vg++;
+            }
+            if (RadioButtonList1.SelectedValue == "Good")
+            {
+                g++;
+            }
+            if (RadioButtonList1.SelectedValue == "Poor")
+            {
+                p++;
+            }
+
+
+            divv.Visible = false;
+            div.Visible = true;
+
+            exx.Text += " " + ex ;
+            vgx.Text +=" "+ vg;
+            gx.Text += " " + g;
+            px.Text += " " + p;
+
+        }
+    }
+    }
