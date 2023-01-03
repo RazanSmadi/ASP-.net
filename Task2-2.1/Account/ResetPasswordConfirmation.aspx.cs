51a9977a434ea6db1686fe774b5c59aaf176fc1a
razansmadi99@gmail.com <razansmadi99@gmail.com>
2023-01-03 10:18:05 +0300

    **

---
+++ Task2-2.1/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace Task2_2._1.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Task2-2.1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Task2_2._1.Global" Language="C#" %>
